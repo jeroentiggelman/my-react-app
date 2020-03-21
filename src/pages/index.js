c6fa9cdeca87942b2983dabc1367f4c9b52c9441
Jeroen Tiggelman <infos@photographicdesign.be>
2020-03-21 19:05:54 +0100

    watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Learn to design and code React apps</h1>
     <p>Complete courses about the beste tools and design systems. Prototype and build apps with React and Swift.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the video!</Link>
   </Layout>
 )
 
