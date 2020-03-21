b22630cd645a55525e7bd10b2bbb1d03e0aee2eb
Jeroen Tiggelman <infos@photographicdesign.be>
2020-03-21 20:56:49 +0100

    first customizing

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,8 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./bootstrap.min.css"
-import "./layout.css"
+import "./bootstrap.min.scss"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -27,13 +27,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div className='belowHeader'>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()} photoGRAPHICdesign — Built with
