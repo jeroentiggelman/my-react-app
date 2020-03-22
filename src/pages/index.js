ca415cbdee768de120f25fd02c23c1ee853af9bc
jeroentiggelman <infos@photographicdesign.be>
2020-03-22 16:30:53 +0100

    basic css with sass added

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete courses about the beste tools and design systems. Prototype and build apps with React and Swift.</p>
-    <Link to="/page-2/">Watch the video!</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the beste tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
